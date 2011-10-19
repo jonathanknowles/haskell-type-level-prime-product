d915580af536cffca94d79ee17747d777fe92215
Jonathan Knowles <mail@jonathanknowles.net>
2011-10-19 21:26:04 +0100

    Rename project from type-level-prime-composition to type-level-prime-product.

--- Data/TypeLevel/PrimeProduct.hs
+++ Data/TypeLevel/PrimeProduct.hs
@@ -7,8 +7,8 @@
 {-# LANGUAGE TypeOperators          #-}
 {-# LANGUAGE UndecidableInstances   #-}
 
--- | Types and functions for type-level prime compositions, encoded as lists of exponentiated prime factors.
-module Data.TypeLevel.PrimeComposition where
+-- | Type-level encodings and operations for products of prime numbers.
+module Data.TypeLevel.PrimeProduct where
 
 infixr 0 :::
 infixl 1 :^:
@@ -22,10 +22,10 @@ data S x
 -- | A type-level representation of an /exponentiated prime number/ : a prime number raised to a positive integer power.
 data prime :^: exponent
 
--- | A type-level representation of a /prime composition/ : a list of exponentiated prime numbers in ascending order [2^/p/, 3^/q/, 5^/r/ ... ].
+-- | A type-level representation of a /prime product/ : a list of exponentiated prime numbers in ascending order [2^/p/, 3^/q/, 5^/r/ ... ].
 data exponentiatedPrime ::: tail
 
--- | A type-level representation of the /empty/ prime composition.
+-- | A type-level representation of the /empty/ prime product.
 data N
 
 -- | The result of a type-level comparison between /a/ and /b/ such that (/a/ = /b/).
@@ -50,7 +50,7 @@ class                   Sub    a     b  c | a b -> c
 instance                Sub    a     Z  a
 instance (Sub a b c) => Sub (S a) (S b) c
 
--- | Converts composition /x/ into a run-time value.
+-- | Converts product /x/ into a run-time value.
 class                          Value      x  where value :: V x
 instance                       Value (    Z) where value  = V (    0)
 instance (Value t         ) => Value (S   t) where value  = V (x + 1) where V x = value :: V t
@@ -88,30 +88,30 @@ instance              Max    Z  (S b) (S b)
 instance              Max (S a)    Z  (S a)
 instance Max a b c => Max (S a) (S b) (S c)
 
--- | Normalises composition /x/ by removing all factors with a zero exponent.
+-- | Normalises product /x/ by removing all factors with a zero exponent.
 class                     Normalise                x                 y | x -> y
 instance                  Normalise                N                 N
 instance Normalise y z => Normalise (a:^:(S x) ::: y) (a:^:(S x) ::: z)
 instance Normalise y z => Normalise (a:^:   Z  ::: y)                z
 
--- | Finds the greatest common divisor of composition /x/ and composition /y/.
+-- | Finds the greatest common divisor of product /x/ and product /y/.
 class                            GCD x y z | x y -> z
 instance (C x y c, G c x y z) => GCD x y z
 
--- | Finds the least common multiple of composition /x/ and composition /y/.
+-- | Finds the least common multiple of product /x/ and product /y/.
 class                            LCM x y z | x y -> z
 instance (C x y c, L c x y z) => LCM x y z
 
--- | Divides composition /x/ by composition /y/ (assuming that /x/ is divisible by /y/).
+-- | Divides product /x/ by product /y/ (assuming that /x/ is divisible by /y/).
 class                                             Divide x y z | x y -> z
 instance (C x y c, D c x y z', Normalise z' z) => Divide x y z
 
--- | Multiplies composition /x/ with composition /y/.
+-- | Multiplies product /x/ with product /y/.
 class                            Multiply x y z | x y -> z
 instance (C x y c, M c x y z) => Multiply x y z
 
--- | Finds the greatest common divisor of composition /x/ and composition /y/, where
---   the heads /a/ and /b/ of compositions /x/ and /y/ have relative ordering /c/.
+-- | Finds the greatest common divisor of product /x/ and product /y/, where
+--   the heads /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             G  c          x           y           z | c x y -> z
 instance                                                          G EQ          N           N           N
 instance                                                          G LT          N  (b:^:q:::y)          N
@@ -120,8 +120,8 @@ instance (C  x          y  c, G c  x          y  z, Min p q r) => G EQ (a:^:p:::
 instance (C  x (b:^:q:::y) c, G c  x (b:^:q:::y) z           ) => G LT (a:^:p:::x) (b:^:q:::y)          z
 instance (C (a:^:p:::x) y  c, G c (a:^:p:::x) y  z           ) => G GT (a:^:p:::x) (b:^:q:::y)          z
 
--- | Finds the least common multiple of composition /x/ and composition /y/, where
---   the heads /a/ and /b/ of compositions /x/ and /y/ have relative ordering /c/.
+-- | Finds the least common multiple of product /x/ and product /y/, where
+--   the heads /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             L  c          x           y           z | c x y -> z
 instance                                                          L EQ          N           N           N
 instance                                                          L LT          N  (b:^:q:::y) (b:^:q:::y)
@@ -130,8 +130,8 @@ instance (C  x          y  c, L c  x          y  z, Max p q r) => L EQ (a:^:p:::
 instance (C  x (b:^:q:::y) c, L c  x (b:^:q:::y) z           ) => L LT (a:^:p:::x) (b:^:q:::y) (a:^:p:::z)
 instance (C (a:^:p:::x) y  c, L c (a:^:p:::x) y  z           ) => L GT (a:^:p:::x) (b:^:q:::y) (b:^:q:::z)
 
--- | Divides composition /x/ by composition /y/ (assuming that /x/ is divisible by /y/),
---   where the heads /a/ and /b/ of compositions /x/ and /y/ have relative ordering /c/.
+-- | Divides product /x/ by product /y/ (assuming that /x/ is divisible by /y/),
+--   where the heads /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             D  c          x           y           z | c x y -> z
 instance                                                          D EQ          N           N           N
 instance                                                          D LT          N  (b:^:q:::y) (b:^:q:::y)
@@ -140,8 +140,8 @@ instance (C  x          y  c, D c  x          y  z, Sub p q r) => D EQ (a:^:p:::
 instance (C  x (b:^:q:::y) c, D c  x (b:^:q:::y) z           ) => D LT (a:^:p:::x) (b:^:q:::y) (a:^:p:::z)
 instance (C (a:^:p:::x) y  c, D c (a:^:p:::x) y  z           ) => D GT (a:^:p:::x) (b:^:q:::y) (b:^:q:::z)
 
--- | Multiplies composition /x/ with composition /y/, where the heads
---   /a/ and /b/ of compositions /x/ and /y/ have relative ordering /c/.
+-- | Multiplies product /x/ with product /y/, where the heads
+--   /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             M  c          x           y           z | c x y -> z
 instance                                                          M EQ          N           N           N
 instance                                                          M LT          N  (b:^:q:::y) (b:^:q:::y)
@@ -150,8 +150,8 @@ instance (C  x          y  c, M c  x          y  z, Add p q r) => M EQ (a:^:p:::
 instance (C  x (b:^:q:::y) c, M c  x (b:^:q:::y) z           ) => M LT (a:^:p:::x) (b:^:q:::y) (a:^:p:::z)
 instance (C (a:^:p:::x) y  c, M c (a:^:p:::x) y  z           ) => M GT (a:^:p:::x) (b:^:q:::y) (b:^:q:::z)
 
--- | Compares the head (/a/ ^ /p/) of composition /x/
---   with the head (/b/ ^ /q/) of composition /y/.
+-- | Compares the head (/a/ ^ /p/) of product /x/
+--   with the head (/b/ ^ /q/) of product /y/.
 --
 --   Returns:
 --
