27bf8a8f9492f6724f8aa07b2b43c9398d6c2307
Jonathan Knowles <mail@jonathanknowles.net>
2011-10-21 00:00:05 +0100

    Rename function 'Decapitate' to 'TrimHead', and function 'Normalize' to 'TrimTail'.

--- Data/TypeLevel/PrimeProduct/Dense.hs
+++ Data/TypeLevel/PrimeProduct/Dense.hs
@@ -27,8 +27,8 @@ data E
 data V x = V Integer
 
 -- | Finds the greatest common divisor of product /x/ and product /y/.
-class                                  GCD x y z | x y -> z
-instance (G x y z', Normalise z' z) => GCD x y z
+class                                 GCD x y z | x y -> z
+instance (G x y z', TrimTail z' z) => GCD x y z
 
 -- | Finds the least common multiple of product /x/ and product /y/.
 class                              LCM      x       y       z | x y -> z
@@ -38,8 +38,8 @@ instance                           LCM (p:::x)      E  (p:::x)
 instance (LCM x y z, Max p q r) => LCM (p:::x) (q:::y) (r:::z)
 
 -- | Divides product /x/ by product /y/ (assuming that /x/ is divisible by /y/).
-class                                  Divide x y z | x y -> z
-instance (D x y z', Normalise z' z) => Divide x y z
+class                                 Divide x y z | x y -> z
+instance (D x y z', TrimTail z' z) => Divide x y z
 
 -- | Multiplies product /x/ with product /y/.
 class                                   Multiply      x       y       z | x y -> z
@@ -48,22 +48,22 @@ instance                                Multiply      E  (q:::y) (q:::y)
 instance                                Multiply (p:::x)      E  (p:::x)
 instance (Multiply x y z, Add p q r) => Multiply (p:::x) (q:::y) (r:::z)
 
--- | Normalises product /x/. Removes all trailing zero exponents.
-class                                                  Normalise x y | x -> y
-instance (Reverse x a, Decapitate a b, Reverse b y) => Normalise x y
+-- | Removes the longest-possible consecutive sequence of zero exponents from the head of the list that encodes product /x/.
+class                    TrimHead          x           y | x -> y
+instance                 TrimHead          E           E
+instance TrimHead x y => TrimHead (   Z :::x)          y
+instance                 TrimHead ((P a):::x) ((P a):::x)
 
--- | Iteratively removes zero exponents from the head of product /x/.
-class                      Decapitate          x           y | x -> y
-instance                   Decapitate          E           E
-instance Decapitate x y => Decapitate (   Z :::x)          y
-instance                   Decapitate ((P a):::x) ((P a):::x)
+-- | Removes the longest-possible consecutive sequence of zero exponents from the tail of the list that encodes product /x/.
+class                                                TrimTail x y | x -> y
+instance (Reverse x a, TrimHead a b, Reverse b y) => TrimTail x y
 
 -- | Reverses the list that encodes product /x/.
 class               Reverse x y | x -> y
 instance R x E y => Reverse x y
 
 -- | Divides product /x/ by product /y/ (assuming that /x/ is divisible by /y/),
---   producing a result that may require normalisation with 'Normalise'.
+--   producing a result that may contain trailing zero exponents.
 class                            D      x       y       z | x y -> z
 instance                         D      E       E       E
 instance (D E y E           ) => D      E  (Z:::y)      E
@@ -71,7 +71,7 @@ instance                         D (p:::x)      E  (p:::x)
 instance (D x y z, Sub p q r) => D (p:::x) (q:::y) (r:::z)
 
 -- | Finds the greatest common divisor of product /x/ and product /y/.
---   producing a result that may require normalisation with 'Normalise'.
+--   producing a result that may contain trailing zero exponents.
 class                            G      x       y       z | x y -> z
 instance                         G      E       E       E
 instance                         G      E  (q:::y)      E
