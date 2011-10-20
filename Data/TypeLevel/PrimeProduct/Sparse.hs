7da083cfd687a822749250e2fa9aec0354edd1f7
Jonathan Knowles <mail@jonathanknowles.net>
2011-10-20 09:18:58 +0100

    Rename identifier 'N' to 'E' in Data.TypeLevel.PrimeProduct.Sparse.

--- Data/TypeLevel/PrimeProduct/Sparse.hs
+++ Data/TypeLevel/PrimeProduct/Sparse.hs
@@ -26,7 +26,7 @@ data prime :^: exponent
 data exponentiatedPrime ::: tail
 
 -- | A type-level representation of the /empty/ prime product.
-data N
+data E
 
 -- | The result of a type-level comparison between /a/ and /b/ such that (/a/ = /b/).
 data EQ
@@ -54,7 +54,7 @@ instance (Sub a b c) => Sub (S a) (S b) c
 class                          Value      x  where value :: V x
 instance                       Value (    Z) where value  = V (    0)
 instance (Value t         ) => Value (S   t) where value  = V (x + 1) where V x = value :: V t
-instance                       Value (    N) where value  = V (    1)
+instance                       Value (    E) where value  = V (    1)
 instance (Value a, Value p) => Value (a:^:p) where value  = V (a ^ p) where (V a, V p) = (value, value) :: (V a, V p)
 instance (Value x, Value y) => Value (x:::y) where value  = V (x * y) where (V x, V y) = (value, value) :: (V x, V y)
 
@@ -90,7 +90,7 @@ instance Max a b c => Max (S a) (S b) (S c)
 
 -- | Normalises product /x/ by removing all factors with a zero exponent.
 class                     Normalise                x                 y | x -> y
-instance                  Normalise                N                 N
+instance                  Normalise                E                 E
 instance Normalise y z => Normalise (a:^:(S x) ::: y) (a:^:(S x) ::: z)
 instance Normalise y z => Normalise (a:^:   Z  ::: y)                z
 
@@ -113,9 +113,9 @@ instance (C x y c, M c x y z) => Multiply x y z
 -- | Finds the greatest common divisor of product /x/ and product /y/, where
 --   the heads /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             G  c          x           y           z | c x y -> z
-instance                                                          G EQ          N           N           N
-instance                                                          G LT          N  (b:^:q:::y)          N
-instance                                                          G GT (a:^:p:::x)          N           N
+instance                                                          G EQ          E           E           E
+instance                                                          G LT          E  (b:^:q:::y)          E
+instance                                                          G GT (a:^:p:::x)          E           E
 instance (C  x          y  c, G c  x          y  z, Min p q r) => G EQ (a:^:p:::x) (a:^:q:::y) (a:^:r:::z)
 instance (C  x (b:^:q:::y) c, G c  x (b:^:q:::y) z           ) => G LT (a:^:p:::x) (b:^:q:::y)          z
 instance (C (a:^:p:::x) y  c, G c (a:^:p:::x) y  z           ) => G GT (a:^:p:::x) (b:^:q:::y)          z
@@ -123,9 +123,9 @@ instance (C (a:^:p:::x) y  c, G c (a:^:p:::x) y  z           ) => G GT (a:^:p:::
 -- | Finds the least common multiple of product /x/ and product /y/, where
 --   the heads /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             L  c          x           y           z | c x y -> z
-instance                                                          L EQ          N           N           N
-instance                                                          L LT          N  (b:^:q:::y) (b:^:q:::y)
-instance                                                          L GT (a:^:p:::x)          N  (a:^:p:::x)
+instance                                                          L EQ          E           E           E
+instance                                                          L LT          E  (b:^:q:::y) (b:^:q:::y)
+instance                                                          L GT (a:^:p:::x)          E  (a:^:p:::x)
 instance (C  x          y  c, L c  x          y  z, Max p q r) => L EQ (a:^:p:::x) (a:^:q:::y) (a:^:r:::z)
 instance (C  x (b:^:q:::y) c, L c  x (b:^:q:::y) z           ) => L LT (a:^:p:::x) (b:^:q:::y) (a:^:p:::z)
 instance (C (a:^:p:::x) y  c, L c (a:^:p:::x) y  z           ) => L GT (a:^:p:::x) (b:^:q:::y) (b:^:q:::z)
@@ -133,9 +133,9 @@ instance (C (a:^:p:::x) y  c, L c (a:^:p:::x) y  z           ) => L GT (a:^:p:::
 -- | Divides product /x/ by product /y/ (assuming that /x/ is divisible by /y/),
 --   where the heads /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             D  c          x           y           z | c x y -> z
-instance                                                          D EQ          N           N           N
-instance                                                          D LT          N  (b:^:q:::y) (b:^:q:::y)
-instance                                                          D GT (a:^:p:::x)          N  (a:^:p:::x)
+instance                                                          D EQ          E           E           E
+instance                                                          D LT          E  (b:^:q:::y) (b:^:q:::y)
+instance                                                          D GT (a:^:p:::x)          E  (a:^:p:::x)
 instance (C  x          y  c, D c  x          y  z, Sub p q r) => D EQ (a:^:p:::x) (a:^:q:::y) (a:^:r:::z)
 instance (C  x (b:^:q:::y) c, D c  x (b:^:q:::y) z           ) => D LT (a:^:p:::x) (b:^:q:::y) (a:^:p:::z)
 instance (C (a:^:p:::x) y  c, D c (a:^:p:::x) y  z           ) => D GT (a:^:p:::x) (b:^:q:::y) (b:^:q:::z)
@@ -143,9 +143,9 @@ instance (C (a:^:p:::x) y  c, D c (a:^:p:::x) y  z           ) => D GT (a:^:p:::
 -- | Multiplies product /x/ with product /y/, where the heads
 --   /a/ and /b/ of products /x/ and /y/ have relative ordering /c/.
 class                                                             M  c          x           y           z | c x y -> z
-instance                                                          M EQ          N           N           N
-instance                                                          M LT          N  (b:^:q:::y) (b:^:q:::y)
-instance                                                          M GT (a:^:p:::x)          N  (a:^:p:::x)
+instance                                                          M EQ          E           E           E
+instance                                                          M LT          E  (b:^:q:::y) (b:^:q:::y)
+instance                                                          M GT (a:^:p:::x)          E  (a:^:p:::x)
 instance (C  x          y  c, M c  x          y  z, Add p q r) => M EQ (a:^:p:::x) (a:^:q:::y) (a:^:r:::z)
 instance (C  x (b:^:q:::y) c, M c  x (b:^:q:::y) z           ) => M LT (a:^:p:::x) (b:^:q:::y) (a:^:p:::z)
 instance (C (a:^:p:::x) y  c, M c (a:^:p:::x) y  z           ) => M GT (a:^:p:::x) (b:^:q:::y) (b:^:q:::z)
@@ -162,9 +162,9 @@ instance (C (a:^:p:::x) y  c, M c (a:^:p:::x) y  z           ) => M GT (a:^:p:::
 --   * 'EQ' if (/a/  = /b/).
 --
 class                                   C              x               y   c | x y -> c
-instance                                C              N               N  EQ
-instance                                C              N  (   b :^:q:::y) LT
-instance                                C (   a :^:p:::x)              N  GT
+instance                                C              E               E  EQ
+instance                                C              E  (   b :^:q:::y) LT
+instance                                C (   a :^:p:::x)              E  GT
 instance                                C (   Z :^:p:::x) (   Z :^:q:::y) EQ
 instance                                C (   Z :^:p:::x) ((S b):^:q:::y) LT
 instance                                C ((S a):^:p:::x) (   Z :^:q:::y) GT
