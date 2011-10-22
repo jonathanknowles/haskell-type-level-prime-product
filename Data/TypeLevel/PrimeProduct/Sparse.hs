057bf57e4771affc6b60f04b8fbfeec47b5ead79
Jonathan Knowles <mail@jonathanknowles.net>
2011-10-22 11:27:31 +0100

    Add mutually-recursive type classes "Expand" and "Expand'".

--- Data/TypeLevel/PrimeProduct/Sparse.hs
+++ Data/TypeLevel/PrimeProduct/Sparse.hs
@@ -42,6 +42,27 @@ instance                  Contract                E                 E
 instance Contract x y => Contract (a:^:(P n) ::: x) (a:^:(P n) ::: y)
 instance Contract x y => Contract (a:^:   Z  ::: x)                y
 
+-- | Expands products /x/ and /y/ into products /x'/ and /y'/ whose
+--   encodings are the same length and share the same set of prime
+--   bases as one another.
+class Expand x x' y y' | x y -> x' y'
+
+instance Expand E E E E
+instance Expand x x E y => Expand (a:^:p:::x) (a:^:p:::x)          E  (a:^:Z:::y)
+instance Expand E x y y => Expand          E  (b:^:Z:::x) (b:^:q:::y) (b:^:q:::y)
+
+instance (Compare a b c, Expand' c (a:^:p:::x) x' (b:^:q:::y) y') => Expand (a:^:p:::x) x' (b:^:q:::y) y'
+
+-- | Expands non-empty products /x/ and /y/ with leading exponentials
+--   /a/^/p/ and /b/^/q/ where /a/ and /b/ have relative ordering /c/
+--   into products /x'/ and /y'/, whose encodings are the same length
+--   and share the same set of prime bases as one another.
+class Expand' c x x' y y' | c x y -> x' y'
+
+instance Expand          x  x'          y  y' => Expand' EQ (a:^:p:::x) (a:^:p:::x') (b:^:q:::y) (b:^:q:::y')
+instance Expand          x  x' (b:^:q:::y) y' => Expand' LT (a:^:p:::x) (a:^:p:::x') (b:^:q:::y) (a:^:Z:::y')
+instance Expand (a:^:p:::x) x'          y  y' => Expand' GT (a:^:p:::x) (b:^:Z:::x') (b:^:q:::y) (b:^:q:::y')
+
 -- | Multiplies product /x/ with product /y/.
 class                                            Multiply x y z | x y -> z
 instance (C x y c, M c x y z', Contract z' z) => Multiply x y z
