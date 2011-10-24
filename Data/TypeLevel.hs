177bc1421bba984cb5e9836856ccf35a5a98e7cd
Jonathan Knowles <mail@jonathanknowles.net>
2011-10-24 08:34:56 +0100

    Rearrange module Data.TypeLevel.

--- Data/TypeLevel.hs
+++ Data/TypeLevel.hs
@@ -5,31 +5,44 @@
 {-# LANGUAGE UndecidableInstances   #-}
 
 module Data.TypeLevel
-	( Compare
-	, EQ
+	( EQ
 	, LT
 	, GT
-	, Maximum
-	, Minimum
-	, Negate
-	, Add
-	, Subtract
-	, Multiply
-	, Divide
-	, LCM
-	, GCD
-	, Reciprocal
+	, ApplyUnary
 	, ApplyBinary
-	, OperatorCompare
-	, OperatorAdd
-	, OperatorSubtract
-	, OperatorMaximum
-	, OperatorMinimum
-	, OperatorMultiply
-	, OperatorDivide
-	, OperatorLCM
-	, OperatorGCD
+	, Negate     , OperatorNegate
+	, Reciprocal , OperatorReciprocal
+	, Compare    , OperatorCompare
+	, Maximum    , OperatorMaximum
+	, Minimum    , OperatorMinimum
+	, Add        , OperatorAdd
+	, Subtract   , OperatorSubtract
+	, Multiply   , OperatorMultiply
+	, Divide     , OperatorDivide
+	, LCM        , OperatorLCM
+	, GCD        , OperatorGCD
 	) where
+
+-- | The result of a comparison between /a/ and /b/ such that (/a/ = /b/).
+data EQ
+
+-- | The result of a comparison between /a/ and /b/ such that (/a/ \< /b/).
+data LT
+
+-- | The result of a comparison between /a/ and /b/ such that (/a/ > /b/).
+data GT
+
+-- Apply unary operator /f/ to /a/ to produce /b/.
+class ApplyUnary f a b | f a -> b
+
+-- Apply binary operator /f/ to /a/ and /b/ to produce /c/.
+class ApplyBinary f a b c | f a b -> c
+
+-- | Finds the negation /b/ of /a/.
+class Negate a b | a -> b
+
+-- | Finds the reciprocal /b/ of /a/.
+class Reciprocal a b | a -> b
 
 -- | Compares /a/ with /b/ to find their relative order /c/.
 --
@@ -43,25 +56,25 @@ module Data.TypeLevel
 --
 class Compare a b c | a b -> c
 
--- | The result of a comparison between /a/ and /b/ such that (/a/ = /b/).
-data EQ
-
--- | The result of a comparison between /a/ and /b/ such that (/a/ \< /b/).
-data LT
-
--- | The result of a comparison between /a/ and /b/ such that (/a/ > /b/).
-data GT
-
 -- | Finds the greater of /a/ and /b/.
 class Maximum a b c | a b -> c
 instance (Compare a b z, Maximum' z a b c) => Maximum a b c
+
+-- | Finds the greater of /a/ and /b/ when the relative ordering /z/ between /a/ and /b/ is known.
+class    Maximum' z  a b c | z a b -> c
+instance Maximum' EQ a a a
+instance Maximum' LT a b b
+instance Maximum' GT a b a
 
 -- | Finds the smaller of /a/ and /b/.
 class Minimum a b c | a b -> c
 instance (Compare a b z, Minimum' z a b c) => Minimum a b c
 
--- | Finds the negation /b/ of /a/.
-class Negate a b | a -> b
+-- | Finds the smaller of /a/ and /b/ when the relative ordering /z/ between /a/ and /b/ is known.
+class    Minimum' z  a b c | z a b -> c
+instance Minimum' EQ a a a
+instance Minimum' LT a b a
+instance Minimum' GT a b b
 
 -- | Adds /a/ to /b/ to produce /c/.
 class Add a b c | a b -> c
@@ -82,11 +95,11 @@ class LCM a b c | a b -> c
 -- | Finds the greatest common divisor /c/ of /a/ and /b/.
 class GCD a b c | a b -> c
 
--- | Finds the reciprocal /b/ of /a/.
-class Reciprocal a b | a -> b
+data OperatorNegate
+data OperatorReciprocal
 
--- Apply binary operator /f/ to /a/ and /b/ to produce /c/.
-class ApplyBinary f a b c | f a b -> c
+instance Negate     a b => ApplyUnary OperatorNegate     a b
+instance Reciprocal a b => ApplyUnary OperatorReciprocal a b
 
 data OperatorCompare
 data OperatorAdd
@@ -107,16 +120,4 @@ instance Multiply a b c => ApplyBinary OperatorMultiply a b c
 instance Divide   a b c => ApplyBinary OperatorDivide   a b c
 instance LCM      a b c => ApplyBinary OperatorLCM      a b c
 instance GCD      a b c => ApplyBinary OperatorGCD      a b c
-
--- | Finds the greater of /a/ and /b/ when the relative ordering /z/ between /a/ and /b/ is known.
-class    Maximum' z  a b c | z a b -> c
-instance Maximum' EQ a a a
-instance Maximum' LT a b b
-instance Maximum' GT a b a
-
--- | Finds the smaller of /a/ and /b/ when the relative ordering /z/ between /a/ and /b/ is known.
-class    Minimum' z  a b c | z a b -> c
-instance Minimum' EQ a a a
-instance Minimum' LT a b a
-instance Minimum' GT a b b
 
